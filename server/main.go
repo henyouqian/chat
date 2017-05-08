457c065019d8c57c1a34835273c2f056a5d50a1e
李炜 <henyouqian@gmail.com>
2017-05-09 01:05:14 +0800

    first commit

---
+++ server/main.go
@@ -0,0 +1,41 @@
+// liwei
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/golang/glog"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+
+func main() {
+	flag.Parse()
+	glog.Info("server running")
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
